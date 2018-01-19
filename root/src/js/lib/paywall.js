2bd51720a7cefb36b62f4cb6258fb586e9b4cffa
Robyn Meshulam <robyn.meshulam@gmail.com>
2018-01-19 12:50:54 -0800

    Include paywall config location in setup variables

--- root/src/js/lib/paywall.js
+++ root/src/js/lib/paywall.js
@@ -7,12 +7,13 @@ var paywall = function(pageId) {
 
   window.SEATIMESCO.paywall.contentMetered = true;
   window.SEATIMESCO.paywall.pageExcluded = false;
+  window.SEATIMESCO.paywall.configsPath = "https://www.seattletimes.com/wp-json/paywall/settings";
 
   window.SEATIMESCO.contentInfo.domain = "projects.seattletimes.com";
   window.SEATIMESCO.contentInfo.post_id = pageId;
-  window.SEATIMESCO.contentInfo.sections_all = 'projects';
+  window.SEATIMESCO.contentInfo.sections_all = "projects";
 
-  var paywallScript = document.createElement('script');
+  var paywallScript = document.createElement("script");
   paywallScript.async = true;
   paywallScript.defer = true;
   paywallScript.src = "https://www.seattletimes.com/wp-content/plugins/st-user-messaging/dist/st-user-messaging-paywall-bundle.js";
