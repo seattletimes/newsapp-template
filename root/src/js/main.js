a963006244bf7ed05a5f40559c18abf64cd8de8f
emilymeng <eeng@seattletimes.com>
2022-07-19 17:15:36 -0700

    side-chain replaces responsive child

--- root/src/js/main.js
+++ root/src/js/main.js
@@ -1,4 +1,14 @@
 // var paywall = require("./lib/paywall");
 // setTimeout(() => paywall(12345678), 5000);
 
-require("component-responsive-frame/child");
+//added sidechain 
+import Sidechain from '@nprapps/sidechain';
+const guest = Sidechain.registerGuest({ sentinel: 'st' });
+
+guest.sendMessage({
+  type: 'analytics',
+  eventCategory: 'interaction',
+  eventAction: 'click',
+  eventLabel: 'etc'
+})
+//end sidechain
